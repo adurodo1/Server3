c16eba98f38aa5740b6ea9cad47c5e805af8fb15
kingO1989 <77279327+kingO1989@users.noreply.github.com>
2023-08-23 04:38:57 -0400

    new commits 3 , /home page

---
+++ __test__/users.test.js
@@ -0,0 +1,90 @@
+const request=require("supertest");
+var db= require("../dbConnect")
+var app= require("../app");
+let session = null;
+
+
+
+ 
+
+describe('Our server', function() {
+    // Called once before any of the tests in this block begin.
+
+    beforeEach(async()=>{
+   
+    
+  
+
+    });
+    
+    
+    test('Testing Routes',()=>{
+        request(app).get('/loginerror').expect(200).end(function(err, res) {
+            if (err) throw err;
+            
+      });
+
+      
+
+    })
+    
+    
+    test('Sign up user',(done)=>{
+       
+         request.agent(app).post('/register')
+        .send({
+    
+            "username":"ggggggggggggggggkk",
+            "userpassword":"dfgdfgfdgfdgdf"
+        }).expect(201).end((err, res) => {  
+            if (err) {
+               done(err);
+            }
+            console.log(res.header['set-cookie']);
+            session = res.header['set-cookie'];  
+            done();  
+        })
+        
+    });
+    
+    test('login',(done)=>{
+       
+         request(app).post('/login')
+        .send({
+    
+            "username":"dfgdfgfdg",
+            "userpassword":"dfgdfgfdgfdgdf"
+        }).expect(302).end((err, res) => {  
+            if (err) {
+               done(err);
+            }
+            console.log(res.header['set-cookie']);
+            session = res.header['set-cookie'];  
+            done();  
+        })
+    })
+    
+    
+    
+    test('profile',async()=>{
+    
+        await request(app).post('/login')
+        .send({
+    
+            "username":"dfgdfgfdg",
+            "userpassword":"dfgdfgfdgfdgdf"
+        }) 
+       
+        await request(app).get('/profile')
+        .expect(302)
+        .expect('Content-Type', 'text/plain; charset=utf-8')
+        
+     
+    
+    
+    })
+
+
+
+})
+
